1bc4ad885ec7d30d6fc6cb0e1c5fc2959b8f3c3f
ayushbishtcode <ayushbisht044@gmail.com>
2026-08-18 20:17:06 +0530

    architectureof kubernetes priject added

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "application": "Kubernetes Flask Demo",
+        "version": os.getenv("APP_VERSION", "v1"),
+        "pod": socket.gethostname(),
+        "status": "running"
+    }
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5006)
